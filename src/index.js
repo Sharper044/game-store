920e0361b87ab097b082258f0fc002d17bb76e73
Stuart Harper <stuartharper044@gmail.com>
2018-10-20 14:27:04 -0600

    gutted typescript to save time

--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import App from './App';
-import './reset.css';
 import store from './store';
+import 'typeface-roboto';
 
+import './reset.css';
 import './index.css';
 
 ReactDOM.render(
